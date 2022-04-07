e5030872461691f3208aef4be39a37a2f0eb9545
Lexi Dorrity <lexidorrity@Lexis-MBP.socal.rr.com>
2022-04-07 07:43:06 -0700

    add schema

--- db/schema.sql
+++ db/schema.sql
@@ -5,14 +5,24 @@ USE employee_db;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  department_name VARCHAR(30) NOT NULL
+  name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE roles (
+CREATE TABLE role (
    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
    title VARCHAR(30),
    salary DECIMAL,
-   department_id INT,
-   PRIMARY KEY (id)
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 );
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30)
+    last_name VARCHAR(30)
+    role_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES manager(id)
+    ON DELETE SET NULL
+)
 
